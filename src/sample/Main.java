2cea1023a6df609002df42c9b561e99718828cbc
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-02-22 01:56:12 +0530

    Change the title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("UI control");
+        primaryStage.setTitle("Cabin Booking System");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
